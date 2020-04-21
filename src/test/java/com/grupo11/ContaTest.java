79f9f5d17cea7a33a013e910792a897bc1cf8a3d
Luis Lima <luis.contato01@gmail.com>
2020-04-21 20:09:16 -0300

    conta and tests done

--- src/test/java/com/grupo11/ContaTest.java
+++ src/test/java/com/grupo11/ContaTest.java
@@ -99,4 +99,72 @@ public class ContaTest {
         this.cGold.deposito(valor);
         Assertions.assertEquals(Categoria.PLATINUM, this.cGold.getStatus());
     }
+
+    @Test
+    public void testeDepositoPlatinum() throws InvalidValueException{
+        
+        valor = 1000;
+        this.cPlatinum.deposito(valor);
+        Assertions.assertEquals(Categoria.GOLD, this.cPlatinum.getStatus());
+
+        this.cPlatinum = new Conta(Categoria.PLATINUM, 200000, "Luís Lima");
+        valor = 1;
+        this.cPlatinum.deposito(valor);
+        Assertions.assertEquals(Categoria.PLATINUM, this.cPlatinum.getStatus());
+
+    }
+
+
+    @Test
+    public void testeSaqueZerado() throws InvalidValueException{
+        valor = 1;
+        Assertions.assertThrows(InvalidValueException.class, () -> this.cPlatinum.retirada(valor));
+        Assertions.assertThrows(InvalidValueException.class, () -> this.cGold.retirada(valor));
+        Assertions.assertThrows(InvalidValueException.class, () -> this.cSilver.retirada(valor));
+        
+    }
+
+    @Test
+    public void testeSaqueNegativo() throws InvalidValueException{
+        valor = -100;
+        Assertions.assertThrows(InvalidValueException.class, () -> this.cPlatinum.retirada(valor));
+        Assertions.assertThrows(InvalidValueException.class, () -> this.cGold.retirada(valor));
+        Assertions.assertThrows(InvalidValueException.class, () -> this.cSilver.retirada(valor));
+
+    }
+
+    @Test 
+    public void testeSaqueSilver() throws InvalidValueException {
+
+        this.cSilver.setSaldo(2000);
+        this.cSilver.retirada(2000);
+        Assertions.assertEquals(Categoria.SILVER, cSilver.getStatus());
+    }
+
+    @Test 
+    public void testeSaqueGold() throws InvalidValueException{
+        /**
+         * gold 1 real, saque(1) -> silver
+         * gold 50000 - saque(1) -> gold
+         * gold 49999 - saque(1) -> silver
+         */
+
+        this.cGold.setSaldo(1);
+        this.cGold.retirada(1);
+        Assertions.assertEquals(Categoria.SILVER, this.cGold.getStatus());
+
+        this.cGold = new Conta(Categoria.GOLD, 50000, "Luis Lima");
+        this.cGold.retirada(1);
+        Assertions.assertEquals(Categoria.GOLD, this.cGold.getStatus());
+
+    
+        this.cGold = new Conta(Categoria.GOLD, 50000, "Adilson Medronha");
+        this.cGold.retirada(50000);
+        Assertions.assertEquals(Categoria.GOLD, this.cGold.getStatus());
+        
+        this.cGold = new Conta(Categoria.GOLD, 24999, "Luis Lima");
+        this.cGold.retirada(1); 
+        Assertions.assertEquals(Categoria.SILVER, this.cGold.getStatus());
+
+    }
 }
